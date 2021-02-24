e966167eeb741551e81053055688cc10e90e377f
hanasmarcin <mahanas@outlook.com>
2021-02-24 14:29:19 +0100

    tensorflow GPU set max RAM to 512MB

--- WallpaperCreator.py
+++ WallpaperCreator.py
@@ -36,7 +36,6 @@ def reset_weights(model):
 # gpus = tf.config.experimental.list_physical_devices('GPU')
 # if gpus:
 #     try:
-#         tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=512)])
 #     except RuntimeError as e:
 #         print(e)
 
@@ -45,6 +44,7 @@ if gpus:
     try:
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
+        tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=512)])
     except RuntimeError as e:
         print(e)
 
